db666de89dfe7627b1655dc67a0f395ea2d85f0e
walidbsh <164235427+walidbsh@users.noreply.github.com>
2024-03-22 00:44:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,30 +6,13 @@ const express = require('express');
 const ytdl = require('ytdl-core');
 const http = require('http');
 
+var cors = require('cors'); 
+
+
 
 const app = express(); 
-
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://your-frontend.com"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
-
-  next();
-});
-
+ 
+app.use(cors());
 app.get("/api/video/:id", async (req, res) => { 
     let id = req.params.id;
     let video_url = "https://www.youtube.com/watch?v=" + id;
