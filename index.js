d7548f482f92b4fe712909649776d01f42b615e2
walidbsh <164235427+walidbsh@users.noreply.github.com>
2024-03-23 11:57:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ const app = express();
 app.use(cors());
 app.get("/api/video/:id", async (req, res) => { 
     let id = req.params.id;
-    let video_url = "https://www.youtube.com/watch?v=" + id;
-
-    ytdl.getInfo(video_url).then(info => { let audioFormat = ytdl.chooseFormat(info.formats, {quality: 'highestaudio', filter: 'audioonly' });
-        res.send( audioFormat.url );   
+    let video_url = "https://www.youtube.com/watch?v=" + id
+ 
+    ytdl.getInfo(video_url).then(info => { 
+        
+     let audioFormat = ytdl.chooseFormat(info.formats, {quality: 'highestaudio', filter: 'audioonly' });
+        res.send( info );   
     });
 });
 const port = process.env.PORT || 5000; 
