a8e4b013b1939dffe66d575205f21d19d1b66812
walidbsh <164235427+walidbsh@users.noreply.github.com>
2024-03-23 09:31:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 //const path = require('path');
 //const bodyParser = require('body-parser');
 //const fs = require('fs');
-const ytdl = require('ytdl-core');
+const ytdl = require('ytdlp-nodejs');
 const http = require('http');
 
 var cors = require('cors'); 
